5dbe042b87bf13661262b3ecf1f46a71957faed8
joshkub <163014412+joshkub@users.noreply.github.com>
2025-02-04 14:38:01 -0600

    Create ip4-3.py

---
+++ ip4-3.py
@@ -0,0 +1,18 @@
+eggs = int(input("Enter # of dozen to be purchased: "))
+price = 0.0
+
+# if dozen > 0 and dozen <= 99:
+if 0 < dozen < 4:
+    price = 0.50
+elif 4 <= dozen < 6:
+    price = 0.45
+elif 6 <= dozen < 11:
+    price = 0.40
+elif dozen >= 11:
+    price = 0.35
+else:
+    print("The # of dozen is invalid!")
+    quit()
+
+print("# of eggs is: " + str(eggs))
+print("The total cost is: $" + str(price * dozen))
