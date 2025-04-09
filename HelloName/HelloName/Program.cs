0adfd28723aca4763a1b8848b53253f791fc44ac
joshkub <163014412+joshkub@users.noreply.github.com>
2025-04-09 15:09:45 -0500

    vector

---
+++ HelloName/HelloName/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+Console.Write("Enter your name: ");
+
+string name = Console.ReadLine();
+Console.WriteLine("Hello, " + name);
+
+Console.Write("Enter your age: ");
+int age = int.Parse(Console.ReadLine());
+Console.WriteLine("You are " + age + " years old!");
