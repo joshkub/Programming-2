e094b9dab3c5fe2d0ddc08111168fed3c98b0fbc
joshkub <163014412+joshkub@users.noreply.github.com>
2025-04-11 15:19:51 -0500

    1

---
+++ Prog52c/Prog52c/Program.cs
@@ -0,0 +1,10 @@
+﻿using static System.Console;
+Write("Enter radius: ");
+double rad = int.Parse(ReadLine());
+
+double area = rad * rad * 3.14159;
+double circ = 2 * 3.14159 * rad;
+WriteLine("Radius: " + rad);
+WriteLine("Area: " + Math.Round(area, 2));
+WriteLine("Circumference: " + Math.Round(circ, 2));
+ReadKey();
