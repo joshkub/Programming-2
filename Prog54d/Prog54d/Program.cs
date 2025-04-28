18a46667853b332a17f510052aa69cf243fb7260
joshkub <163014412+joshkub@users.noreply.github.com>
2025-04-28 14:54:12 -0500

    1

--- Prog54d/Prog54d/Program.cs
+++ Prog54d/Prog54d/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write("Enter length: ");
+int length = int.Parse(Console.ReadLine());
+Console.Write("Enter height: ");
+int height = int.Parse(Console.ReadLine());
+
+double hypo = Math.Round(Math.Sqrt(length * length + height * height), 2);
+int area = (length * height) / 2;
+double peri = Math.Round(hypo + length + height, 2);
+Console.WriteLine("Hypotonuse: " + hypo);
+Console.WriteLine("Area: " + area);
+Console.WriteLine("Perimeter: " + peri);
+
+
+
+
+
