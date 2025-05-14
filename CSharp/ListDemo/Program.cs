ba29e532cc3258c8737a37f093e0e24e8ff96059
joshkub <163014412+joshkub@users.noreply.github.com>
2025-05-14 13:58:47 -0500

    nice

---
+++ CSharp/ListDemo/Program.cs
@@ -0,0 +1,18 @@
+﻿//List<string> text = [];
+List<string> lines = new List<string>();
+// List<int> nums = [1, 2, 3, 4 ];
+List<int> nums = new List<int>() { 1, 2, 3, 4, 15 };
+
+nums.Add(4);
+nums.Add(5);
+nums.Add(6);
+nums.Add(7);
+nums.RemoveAt(5);
+nums.Remove(7);
+Console.WriteLine("Length: " + nums.Count);
+Console.WriteLine(string.Join(", ", nums));
+
+foreach (int n in nums)
+    Console.WriteLine(n);
+
+// Look at C# documentation on MSON for all list methods
